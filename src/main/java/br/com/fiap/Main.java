ff0b30f6bfe2b85fdd77e7141bb66a49ad17a7c0
Benefrancis <benefrancis@gmail.com>
2023-08-09 23:10:35 -0300

    Criei as primeiras entidades do projeto

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -1,7 +1,15 @@
 package br.com.fiap;
 
+import br.com.fiap.domain.entity.Deposito;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        Deposito depositoNaVilaLeopoldinaDoBrasil = new Deposito();
+
+
+        depositoNaVilaLeopoldinaDoBrasil.setId(1L).setNome("Vila Leopoldina");
 
 
 
--- src/main/java/br/com/fiap/domain/entity/ProdutoEstocado.java
+++ src/main/java/br/com/fiap/domain/entity/ProdutoEstocado.java
@@ -1,4 +1,26 @@
 package br.com.fiap.domain.entity;
 
+import java.time.LocalDateTime;
+
 public class ProdutoEstocado {
+
+
+    Long id;
+
+
+    Produto produto;
+
+
+    Deposito deposito;
+
+
+    String numeroDeSerie;
+
+
+    LocalDateTime entrada;
+
+
+    LocalDateTime saida;
+
+
 }
